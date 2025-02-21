0f9a975e37c361750f7779c997a34984c14b817c
Rakshith Rao <77118966+Rakshith12-pixel@users.noreply.github.com>
2025-02-21 15:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("testbutton");
+
+function usage(event) {
+    event.preventDefault();
+    location.replace("/Users/rakshithrao/codes/html/ttspage/index.html");
+  } 
+
+button.addEventListener("click",usage)
+
+function hover(event){
+    event.preventDefault();
+    button.style.backgroundColor = "green";
+}
+
+const gitbutton = document.getElementById("github")
+
+function git(event){
+    event.preventDefault();
+    window.open(
+        "https://github.com/Rakshith12-pixel", "_blank");
+}
+
+
+gitbutton.addEventListener("click",git);
